cc4689bba9dade7263da408351a6d3ce0ad77c9e
SnegurkaWs <snegurka@web-esse.ru>
2022-01-05 18:57:14 +0500

    ADD: dispatch setAuth from component

--- src/components/UserBlock/UserBlock.tsx
+++ src/components/UserBlock/UserBlock.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+
 import styles from "./UserBlock.module.css";
 
 import Button from "../UI/Button/Button";
+import { AppStateType } from "../../store/reducers";
+import setAuth from '../../store/AC/auth';
 
 const UserBlock: React.FC = () => {
+    const { isLoggin } = useSelector((s: AppStateType) => s.auth);
+    const dispatch = useDispatch();
+
     return (
         <div className={styles.wrapper}>
-            <Button appearance='primary'>Login</Button>
+            {isLoggin ? <span>Hi, Demo</span> : <Button appearance='primary' callback={() => { dispatch(setAuth()) }}>Login</Button>}
+
         </div>
     )
 }
