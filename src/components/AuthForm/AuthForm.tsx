c24dd6bb6d2b305c9f38bbe085ac6567ee848fb9
SnegurkaWs <snegurka@web-esse.ru>
2022-06-06 16:13:55 +0500

    ADD: form input to AuthForm

--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import setAuth from '../../store/AC/auth';
@@ -7,13 +7,25 @@ import styles from './AuthForm.module.css';
 
 import Button from "../UI/Button/Button";
 import { createUserDocumentFromAuth, signInWithGooglePopup } from '../../utils/firebase.utils';
+import FormInput from '../UI/FormInput/FormInput';
+
+const defaultFields = {
+    email: '',
+    password: '',
+}
 
 const AuthForm: React.FC = () => {
-    const [username, setUsername] = useState('demo@demo.com');
-    const [password, setPassword] = useState('demodemo');
+    const [fields, setFields] = useState(defaultFields);
+    const { email, password } = fields;
 
     const dispatch = useDispatch();
     const history = useHistory();
+
+    const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+
+        setFields({ ...fields, [name]: value });
+    };
 
     const submitHandler = (evt: React.FormEvent) => {
         evt.preventDefault();
@@ -23,32 +35,16 @@ const AuthForm: React.FC = () => {
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
+        await createUserDocumentFromAuth(user);
     };
 
     return (
         <section>
             <h2>Login</h2>
             <form action="#" method="post" className={styles.wrapper_form}>
-                <label>
-                    <span>Email address</span>
-                    <input
-                        type='text'
-                        value={username}
-                        name='username'
-                        placeholder="example@example.com"
-                        onChange={(evt) => { setUsername(evt.currentTarget.value) }}
-                    />
-                </label>
-                <label>
-                    <span>Password</span>
-                    <input
-                        type='password'
-                        value={password}
-                        name='password'
-                        onChange={(evt) => { setPassword(evt.currentTarget.value) }}
-                    />
-                </label>
+                <FormInput label="Email address" name="email" value={email} onChange={changeHandler} />
+                <FormInput label="Password" name="password" value={password} type='password' onChange={changeHandler} />
+
                 <Button appearance='primary' callback={submitHandler}>Login</Button>
             </form>
             <Button appearance='primary' callback={logGoogleUser}>Sign in with Google Popup</Button>
