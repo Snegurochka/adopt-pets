28bc4aab7348583ecb711050b57f3bf97feb47ec
SnegurkaWs <snegurka@web-esse.ru>
2022-06-09 10:29:20 +0500

    fix: improve oauthoken request

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 // Read more about the API here: https://www.petfinder.com/developers/v2/docs/
 
 const API_URL = 'https://api.petfinder.com/v2/';
-const API_KEY = process.env.REACT_APP_API_KEY || 'testKey';
-const API_S_KEY= process.env.REACT_APP_API_S_KEY || 'btBtW7RD06057BeDvz92jiaCCshnRgJlPMWMI1nU';
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_S_KEY= process.env.REACT_APP_API_S_KEY;
 
 const API_BD_URL = 'https://adopt-me-48d2e-default-rtdb.europe-west1.firebasedatabase.app/';
 
