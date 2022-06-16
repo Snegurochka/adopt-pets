1b28e279059506af991fc7885eb9b23e8a9e8e9f
SnegurkaWs <snegurka@web-esse.ru>
2022-06-16 15:17:57 +0500

    ADD: select favorites animals for Favorites page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Layout from '../../components/Layout/Layout';
+import { selectFavoritesAnimals } from '../../store/selectors/favorites';
 
 const FavoritesPage: React.FC = () => {
+    const animals = useSelector(selectFavoritesAnimals);
     return (
         <Layout typeContent="page" header="Favorites">
-            <div>FavoritesPage</div>
+            {animals.map((animal) => (<div key={animal.id}>
+                {animal.name}
+            </div>))}
         </Layout>
 
     )
