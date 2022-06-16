84bfe9296311efccf7a407b3b9792fe3698d7e43
SnegurkaWs <snegurka@web-esse.ru>
2022-06-16 14:54:02 +0500

    ADD: My account link

--- src/pages/Account/Account.tsx
+++ src/pages/Account/Account.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import Layout from '../../components/Layout/Layout'
+import { AppStateType } from '../../store/reducers';
 
 const Account: React.FC = () => {
+    const { user } = useSelector((s: AppStateType) => s.user);
     return (
         <Layout typeContent="page" header="My account">
-            <div>
-                Account
-            </div>
+            {user
+                ? (<div>
+                    Account
+                </div>)
+                :
+                <Redirect to='/' />
+            }
+
         </Layout>
     )
 }
