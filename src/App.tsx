255ceb30e0a61124c01ce39364b02e15ae6992a7
SnegurkaWs <snegurka@web-esse.ru>
2022-07-12 12:41:17 +0500

    ADD: lazy to Details

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import { lazy, Suspense, useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "./utils/firebase.utils";
 import { setUser } from "./store/AC/user";
 import setAccessToken from "./store/AC/accessToken";
@@ -8,12 +8,14 @@ import API from "./API";
 import { AppRoute } from "./const";
 
 // Pages
-import Home from './pages/Home/Home';
-import AuthPage from './pages/AuthPage/AuthPage';
-import Details from './pages/Details/Details';
-import Account from "./pages/Account/Account";
 import NotFound from "./pages/NotFound/NotFound";
-import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
+import Spinner from "./components/Spinner/Spinner";
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Details = lazy(() => import('./pages/Details/Details'));
+const AuthPage = lazy(() => import('./pages/AuthPage/AuthPage'));
+const Account = lazy(() => import('./pages/Account/Account'));
+const FavoritesPage = lazy(() => import('./pages/FavoritesPage/FavoritesPage'));
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
@@ -40,14 +42,17 @@ const App: React.FC = () => {
 
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path={AppRoute.HOME} exact component={Home} />
-        <Route path={AppRoute.AUTH} component={AuthPage} />
-        <Route path={AppRoute.ACCOUNT} component={Account} />
-        <Route path={AppRoute.DETAILS} component={Details} />
-        <Route path={AppRoute.FAVORITES} component={FavoritesPage} />
-        <Route path='/*' component={NotFound} />
-      </Switch>
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route path={AppRoute.HOME} exact component={Home} />
+          <Route path={AppRoute.AUTH} component={AuthPage} />
+          <Route path={AppRoute.ACCOUNT} component={Account} />
+          <Route path={AppRoute.DETAILS} component={Details} />
+          <Route path={AppRoute.FAVORITES} component={FavoritesPage} />
+          <Route path='/*' component={NotFound} />
+        </Switch>
+      </Suspense>
+
     </BrowserRouter>
   );
 }
