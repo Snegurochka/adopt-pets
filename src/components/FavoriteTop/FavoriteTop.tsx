d0ac34aa13e928cbd574ca90bffbe43cf4148c0c
SnegurkaWs <snegurka@web-esse.ru>
2022-07-15 10:21:11 +0500

    ADD: AppRoute to UserBlock compomemts

--- src/components/FavoriteTop/FavoriteTop.tsx
+++ src/components/FavoriteTop/FavoriteTop.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { selectFavoritesCount } from "../../store/selectors/favorites";
 
+import { AppRoute } from '../../const';
 import styles from "./FavoriteTop.module.css";
 import { ReactComponent as FavoriteIcon } from '../../assets/favorite-icon.svg';
 
@@ -9,10 +11,10 @@ const FavoriteTop: FC = () => {
     const favoritesCount = useSelector(selectFavoritesCount);
 
     return (
-        <div className={styles.wrapper}>
+        <Link to={AppRoute.FAVORITES} className={styles.wrapper}>
             <FavoriteIcon />
             <div className={styles.count}>{favoritesCount}</div>
-        </div>
+        </Link>
     )
 }
 
--- src/components/UserBlock/UserBlock.tsx
+++ src/components/UserBlock/UserBlock.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import { FC } from "react";
 import { Link } from "react-router-dom";
 
 // Redux
 import { useDispatch, useSelector } from "react-redux";
-import { AppStateType } from "../../store/reducers";
+import { selectCurrentUser } from "../../store/selectors/user";
 import { SignOut } from "../../store/AC/user";
 
-
 import styles from "./UserBlock.module.css";
+import { AppRoute } from "../../const";
 
 // components
 import FavoriteTop from "../FavoriteTop/FavoriteTop";
 
 
-const UserBlock: React.FC = () => {
-    const { user } = useSelector((s: AppStateType) => s.user);
+const UserBlock: FC = () => {
+    const user = useSelector(selectCurrentUser);
 
     const dispatch = useDispatch();
 
@@ -27,10 +27,10 @@ const UserBlock: React.FC = () => {
             {user
                 ? (<>
                     <FavoriteTop />
-                    <Link to='/account'>My account</Link>
+                    <Link to={AppRoute.ACCOUNT}>My account</Link>
                     <div onClick={signOutHandler}>Log out</div>
                 </>)
-                : <Link className="btn" to={'/auth'}>Login</Link>
+                : <Link className="btn" to={AppRoute.AUTH}>Login</Link>
             }
         </div>
     )
