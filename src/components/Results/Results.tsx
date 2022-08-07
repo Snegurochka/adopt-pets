2aff28f95a49ce1c4f5135efb42f3fa82219523d
SnegurkaWs <snegurka@web-esse.ru>
2022-08-07 15:59:01 +0500

    ADD: using favorite hook to Pet component

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 import { useSelector } from "react-redux";
-import { selectAnimals, selectAnimalsIsLoading } from "../../store/selectors/animals";
+import {
+  selectAnimals,
+  selectAnimalsIsLoading,
+} from "../../store/selectors/animals";
 import { selectFavoritesAnimals } from "../../store/selectors/favorites";
 
 // components
@@ -9,36 +12,40 @@ import Spinner from "../Spinner/Spinner";
 import Card from "../UI/Card/Card";
 
 const Results: FC = () => {
-    const animals = useSelector(selectAnimals);
-    const isLoading = useSelector(selectAnimalsIsLoading);
-    const favorites = useSelector(selectFavoritesAnimals);
+  const animals = useSelector(selectAnimals);
+  const isLoading = useSelector(selectAnimalsIsLoading);
+  const favorites = useSelector(selectFavoritesAnimals);
 
-    if (isLoading) return (
-        <Card>
-            <Spinner />
-        </Card>);
-
+  if (isLoading)
     return (
-        <Card>
-            {!animals.length ? (<h2>No Pets</h2>) : (
-                animals.map((pet, ind) => {
-                    const favorite = favorites.find((item) => item.id === pet.id);
-
-                    return <Pet
-                        key={`${ind}-${pet.id}`}
-                        id={pet.id}
-                        refId={favorite?.refId || null}
-                        name={pet.name}
-                        animal={pet.type}
-                        breed={pet.breeds.primary}
-                        images={pet.photos}
-                        isFavorite={Boolean(favorite?.id)}
-                    />
-                }
-                )
-            )}
-        </Card>
+      <Card>
+        <Spinner />
+      </Card>
     );
+
+  return (
+    <Card>
+      {!animals.length ? (
+        <h2>No Pets</h2>
+      ) : (
+        animals.map((pet, ind) => {
+          const favorite = favorites.find((item) => item.id === pet.id);
+
+          return (
+            <Pet
+              key={`${ind}-${pet.id}`}
+              id={pet.id}
+              name={pet.name}
+              animal={pet.type}
+              breed={pet.breeds.primary}
+              images={pet.photos}
+              favorite={favorite}
+            />
+          );
+        })
+      )}
+    </Card>
+  );
 };
 
 export default Results;
