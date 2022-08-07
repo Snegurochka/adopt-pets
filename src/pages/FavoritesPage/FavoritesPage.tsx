c7992810456c846a8cd0dca2ee5ee1f19bd91648
SnegurkaWs <snegurka@web-esse.ru>
2022-08-07 17:39:50 +0500

    ADD: remove favorites on favorite page

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -3,14 +3,19 @@ import { useSelector } from "react-redux";
 import { selectFavoritesAnimals } from "../../store/selectors/favorites";
 
 import ContentWrapper from "../../components/Layout/ContentWrapper/ContentWrapper";
+import FavoriteItem from "./FavoriteItem/FavoriteItem";
 
 const FavoritesPage: FC = () => {
   const animals = useSelector(selectFavoritesAnimals);
   return (
     <ContentWrapper header="Favorites">
-      {animals.map((animal) => (
-        <div key={animal.id}>{animal.name}</div>
-      ))}
+      <table>
+        <tbody>
+          {animals.map((animal) => (
+            <FavoriteItem key={animal.id} animal={animal} />
+          ))}
+        </tbody>
+      </table>
     </ContentWrapper>
   );
 };
